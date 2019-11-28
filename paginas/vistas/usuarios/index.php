28d8b43de3c9f7721e568fd396ca6e9526ba9114
carolina <cvelez_jc@hotmail.com>
2019-11-27 22:56:44 -0600

    correccionfe

--- paginas/vistas/usuarios/index.php
+++ paginas/vistas/usuarios/index.php
@@ -8,13 +8,8 @@
 	$nombreModulo = $_POST['nombre'];
 	$rutaModulo = $_POST['ruta'];
 	$idModulo = $_POST['id'];
-	//Agregue
-		$date1 = strtotime("ingreso_usu");  
-		$date2 = strtotime(date("d-m-Y H:i:00",time()));
-	//
 	$usuario = $_SESSION['usuario'];
 	$usuarios = $con->query("Select id_usu as id, CONCAT(IFNULL(nombre_usu,''),' ',IFNULL(apellidop_usu,''), ' ',IFNULL(apellidom_usu,'')) as nombre, nombre_depto, nombre_puesto,ingreso_usu,noempleado_usu as noempleado FROM usuarios u Inner join departamentos on u.depto_usu = id_depto INNER JOIN puestos on u.puesto_usu = id_puesto WHERE status_usu<>0");
-
 	$permisosXUsuarioLoggeado = $con->query("Select nombre_permiso from permisos_modulos pm INNER JOIN permisos_usuarios pu on pu.permiso_pu = pm.id_permiso where pu.status_pu <>0 and pm.status_permiso<>0 and modulo_permiso=".$idModulo. " and usuario_pu = " . $usuario);
 	$permisosActuales = array();
 	while ($permiso = $permisosXUsuarioLoggeado->fetch_assoc()) {
@@ -22,41 +17,7 @@
 	}
 
 	include_once 'header.php';
-
-	// Agregar
-	// Formulate the Difference between two dates 
-$diff = abs($date2 - $date1);  
-  
-  
-// To get the year divide the resultant date into 
-// total seconds in a year (365*60*60*24) 
-$años = floor($diff / (365*60*60*24));  
-  
-  
-// To get the month, subtract it with years and 
-// divide the resultant date into 
-// total seconds in a month (30*60*60*24) 
-$meses = floor(($diff - $años * 365*60*60*24) 
-                               / (30*60*60*24));  
-  
-  
-// To get the day, subtract it with years and  
-// months and divide the resultant date into 
-// total seconds in a days (60*60*24) 
-$dias = floor(($diff - $años * 365*60*60*24 -  
-             $meses*30*60*60*24)/ (60*60*24));
-
-
-
-// Print the result 
-printf("%d años, %d meses, %d dias, "
-     , $años, $meses, 
-             $dias);
-	//
 ?>
-
-
-
 <div id="tabla" >
 	<table class="table DataTable">
 		<thead>
@@ -76,7 +37,19 @@ printf("%d años, %d meses, %d dias, "
 					<td><?php echo $usuario['nombre']; ?></td>
 					<td><?php echo $usuario['nombre_depto']; ?></td>
 					<td><?php echo $usuario['nombre_puesto']; ?></td>
-					<td><?php echo $usuario['ingreso_usu']; ?></td>
+					<td><?php 
+						$date1 = strtotime ($usuario ['ingreso_usu']);  
+						$date2 = strtotime(date("d-m-Y H:i:00",time())); 
+						$diff = abs($date2 - $date1);  
+						$años = floor($diff / (365*60*60*24));  
+						$meses = floor(($diff - $años * 365*60*60*24) 
+                               / (30*60*60*24));  
+						$dias = floor(($diff - $años * 365*60*60*24 -  
+					             $meses*30*60*60*24)/ (60*60*24));
+						printf("%d años, %d meses, %d dias, "
+					     , $años, $meses, 
+					             $dias);
+					 ?></td>
 					<td>
 						<?php if(in_array("EDITAR", $permisosActuales)){ ?>
 							<i class="fas fa-edit"  data-toggle="modal" data-target="#modal" onclick="editarUsuario(<?php echo $usuario['id']; ?>)"></i>
